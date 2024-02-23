c4a9426f222f70efea1c0ea345240e6e4599d912
robots4life <34193243+robots4life@users.noreply.github.com>
2024-02-23 18:48:50 +0100

    markup & styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<div class="calculator">
+		<div class="display">123</div>
+		<div class="digits">
+			<button class="clear span-3">C</button>
+			<button class="operator divide">/</button>
+			<button class="number">1</button>
+			<button class="number">2</button>
+			<button class="number">3</button>
+			<button class="operator multiply">*</button>
+			<button class="number">4</button>
+			<button class="number">5</button>
+			<button class="number">6</button>
+			<button class="operator subtract">-</button>
+			<button class="number">7</button>
+			<button class="number">8</button>
+			<button class="number">9</button>
+			<button class="operator add">+</button>
+			<button class="number span-2">0</button>
+			<button class="dot">.</button>
+			<button class="operator equal">=</button>
+		</div>
+	</div>
+</main>
+
+<style>
+	.calculator {
+		width: 100%;
+	}
+	.display {
+		display: flex;
+		align-items: center;
+		justify-content: end;
+		padding-right: 0.4rem;
+		width: 100%;
+		height: 4rem;
+		font-size: 2rem;
+		font-weight: bold;
+		letter-spacing: 0.2rem;
+		background-color: lightslategrey;
+		color: white;
+		border-radius: 12px;
+		margin-bottom: 1rem;
+	}
+	.digits {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		gap: 1rem;
+	}
+	.span-2 {
+		grid-column: span 2;
+	}
+	.span-3 {
+		grid-column: span 3;
+	}
+	button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 4rem;
+		font-size: 2rem;
+		border: 2px solid black;
+		border-radius: 12px;
+	}
+	.operator {
+		font-size: 3rem;
+		font-weight: bold;
+		background-color: orange;
+	}
+</style>
